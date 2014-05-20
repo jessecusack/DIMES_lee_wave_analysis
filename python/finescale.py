2b2313989d611e2735b012868d0647db27b73a2d
jessecusack <jesse.cusack@noc.soton.ac.uk>
2014-05-20 15:51:34 +0100

    new file, will hold finescale parameterisation functions

---
+++ python/finescale.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 20 15:45:36 2014
+
+A place for finescale parameterisation functions.
+
+@author: jc3e13
+"""
+
+def adiabatic_level():
+    """ """
+    pass
