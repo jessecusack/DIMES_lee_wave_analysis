19f57ce3a0f6e7ca03a6e0fd80710b2bd0d87a42
jessecusack <jesse.cusack@noc.soton.ac.uk>
2014-05-22 16:55:15 +0100

    modified figure settings

--- python/vertical_velocity_analysis.py
+++ python/vertical_velocity_analysis.py
@@ -9,6 +9,7 @@ import emapex
 import vertical_velocity_model as vvm
 import numpy as np
 import plotting_functions as pf
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import pandas as pd
@@ -21,6 +22,11 @@ reload(pf)
 
 def assess_vvm_fit(Float, save_id=''):
     """ """
+
+    font = {'family': 'normal',
+            'weight': 'normal',
+            'size': 7}
+    matplotlib.rc('font', **font)
 
     vfi = Float.__vfi
     hpids = vfi.hpids
@@ -40,7 +46,8 @@ def assess_vvm_fit(Float, save_id=''):
     plt.setp(patches, 'facecolor', 'b', 'alpha', 0.75)
     plt.xlim(np.min(bins), np.max(bins))
     plt.xlabel('$W_w$ (m s$^{-1}$)')
-    title_str = ("Float {}, mean = {:1.2e} m s$^{{-1}}$, std = {:1.2e} "
+    plt.xticks(rotation=45)
+    title_str = ("Float {}\nmean = {:1.2e} m s$^{{-1}}$\nstd = {:1.2e} "
                  "m s$^{{-1}}$").format(floatID, Ww_mean, Ww_std)
     plt.title(title_str)
     name = save_id + '_ww_histogram.pdf'
@@ -60,6 +67,7 @@ def assess_vvm_fit(Float, save_id=''):
     plt.plot(time, Ws)
     plt.ylabel('$W_w$, $W_f$, $W_s$ (m s$^{-1}$)')
     plt.xlabel('Time')
+    plt.xticks(rotation=45)
     title_str = ("Float {}, half profiles {}").format(floatID, hpids[0:N])
     plt.title(title_str)
     plt.legend(['$W_w$', '$W_f$', '$W_s$'])
@@ -84,12 +92,12 @@ def assess_vvm_fit(Float, save_id=''):
     N = len(pnames)
     ticks = np.arange(0.5, N, 1)
     plt.figure(figsize=(3, 3))
-    plt.pcolormesh(vfi.pcorr, cmap=plt.get_cmap('PiYG'))
+    plt.pcolormesh(np.flipud(vfi.pcorr), cmap=plt.get_cmap('PiYG'))
     cbar = plt.colorbar()
     cbar.set_label('Correlation')
     plt.clim(-1, 1)
     plt.xticks(ticks, pnames)
-    plt.yticks(ticks, pnames)
+    plt.yticks(ticks, pnames[::-1])
     title_str = ("Float {}").format(floatID)
     plt.title(title_str)
     name = save_id + '_param_corr.pdf'
@@ -120,22 +128,6 @@ def assess_vvm_fit(Float, save_id=''):
     fname = os.path.join(save_dir, name)
     plt.savefig(fname, format='pdf', bbox_inches='tight')
 
-#    fig, axs = plt.subplots(N, N)#, sharex='col', sharey='row')
-#    for i in xrange(N):
-#        for j in xrange(N):
-#            if i == j:
-#                dat, bins, patches = axs[i, j].hist(vfi.ps[:, i])
-#                plt.setp(patches, 'facecolor', 'b', 'alpha', 0.75)
-#                axs[i, j].set_xlim(np.min(bins), np.max(bins))
-#            else:
-#                axs[i, j].scatter(vfi.ps[:, i], vfi.ps[:, j])
-#
-#            if i == N-1:
-#                axs[i, j].set_xlabel(pnames[j])
-#
-#            if j == 0:
-#                axs[i, j].set_ylabel(pnames[i])
-
     plt.draw()
     plt.show()
 
@@ -151,23 +143,23 @@ except NameError:
     E77 = emapex.EMApexFloat('../../data/EM-APEX/allprofs11.mat', 4977)
 
 
-model = '1'
-cf_key = 'diffsq'
-params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
-fixed = [None, None, None, None, None, None, None]
-vvm.fitter(E76, params0, fixed, model=model, profiles='all', cf_key=cf_key)
-assess_vvm_fit(E76, str(E76.floatID))
-print(E76.__vfi.p)
-
-model = '1'
-cf_key = 'diffsq'
-params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
-fixed = [None, None, None, None, None, None, None]
-vvm.fitter(E77, params0, fixed, model=model, profiles='all', cf_key=cf_key)
-assess_vvm_fit(E77, str(E77.floatID))
-print(E77.__vfi.p)
-
-###############################################################################
+#model = '1'
+#cf_key = 'diffsq'
+#params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
+#fixed = [None, None, None, None, None, None, None]
+#vvm.fitter(E76, params0, fixed, model=model, profiles='all', cf_key=cf_key)
+#assess_vvm_fit(E76, str(E76.floatID))
+#print(E76.__vfi.p)
+#
+#model = '1'
+#cf_key = 'diffsq'
+#params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
+#fixed = [None, None, None, None, None, None, None]
+#vvm.fitter(E77, params0, fixed, model=model, profiles='all', cf_key=cf_key)
+#assess_vvm_fit(E77, str(E77.floatID))
+#print(E77.__vfi.p)
+#
+################################################################################
 
 model = '1'
 cf_key = 'diffsq'
@@ -185,56 +177,56 @@ vvm.fitter(E77, params0, fixed, model=model, profiles='all', cf_key=cf_key)
 assess_vvm_fit(E77, str(E77.floatID)+'_fix_alphakM')
 print(E77.__vfi.p)
 
-###############################################################################
-
-model = '1'
-cf_key = 'diffsq'
-params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
-fixed = [3e-2, None, None, None, None, 16., 27.179]
-vvm.fitter(E76, params0, fixed, model=model, profiles='all', cf_key=cf_key)
-assess_vvm_fit(E76, str(E76.floatID)+'_fix_k0V0M')
-print(E76.__vfi.p)
-
-model = '1'
-cf_key = 'diffsq'
-params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
-fixed = [3e-2, None, None, None, None, 16., 27.179]
-vvm.fitter(E77, params0, fixed, model=model, profiles='all', cf_key=cf_key)
-assess_vvm_fit(E77, str(E77.floatID)+'_fix_k0V0M')
-print(E77.__vfi.p)
-
-###############################################################################
-
-model = '1'
-cf_key = 'diffsq'
-params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
-fixed = [None, None, None, None, 1.156e-6, 16., 27.179]
-vvm.fitter(E76, params0, fixed, model=model, profiles='all', cf_key=cf_key)
-assess_vvm_fit(E76, str(E76.floatID)+'_fix_alphakk0M')
-print(E76.__vfi.p)
-
-model = '1'
-cf_key = 'diffsq'
-params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
-fixed = [None, None, None, None, 1.156e-6, 16., 27.179]
-vvm.fitter(E77, params0, fixed, model=model, profiles='all', cf_key=cf_key)
-assess_vvm_fit(E77, str(E77.floatID)+'_fix_alphakk0M')
-print(E77.__vfi.p)
-
-###############################################################################
-
-model = '1'
-cf_key = 'diffsq'
-params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
-fixed = [3e-2, None, 3.76e-6, None, 1.156e-6, 16., 27.179]
-vvm.fitter(E76, params0, fixed, model=model, profiles='all', cf_key=cf_key)
-assess_vvm_fit(E76, str(E76.floatID)+'_fix_alphakk0V0alphapM')
-print(E76.__vfi.p)
-
-model = '1'
-cf_key = 'diffsq'
-params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
-fixed = [3e-2, None, 3.76e-6, None, 1.156e-6, 16., 27.179]
-vvm.fitter(E77, params0, fixed, model=model, profiles='all', cf_key=cf_key)
-assess_vvm_fit(E77, str(E77.floatID)+'_fix_alphakk0V0alphapM')
-print(E77.__vfi.p)
+################################################################################
+#
+#model = '1'
+#cf_key = 'diffsq'
+#params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
+#fixed = [3e-2, None, None, None, None, 16., 27.179]
+#vvm.fitter(E76, params0, fixed, model=model, profiles='all', cf_key=cf_key)
+#assess_vvm_fit(E76, str(E76.floatID)+'_fix_k0V0M')
+#print(E76.__vfi.p)
+#
+#model = '1'
+#cf_key = 'diffsq'
+#params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
+#fixed = [3e-2, None, None, None, None, 16., 27.179]
+#vvm.fitter(E77, params0, fixed, model=model, profiles='all', cf_key=cf_key)
+#assess_vvm_fit(E77, str(E77.floatID)+'_fix_k0V0M')
+#print(E77.__vfi.p)
+#
+################################################################################
+#
+#model = '1'
+#cf_key = 'diffsq'
+#params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
+#fixed = [None, None, None, None, 1.156e-6, 16., 27.179]
+#vvm.fitter(E76, params0, fixed, model=model, profiles='all', cf_key=cf_key)
+#assess_vvm_fit(E76, str(E76.floatID)+'_fix_alphakk0M')
+#print(E76.__vfi.p)
+#
+#model = '1'
+#cf_key = 'diffsq'
+#params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
+#fixed = [None, None, None, None, 1.156e-6, 16., 27.179]
+#vvm.fitter(E77, params0, fixed, model=model, profiles='all', cf_key=cf_key)
+#assess_vvm_fit(E77, str(E77.floatID)+'_fix_alphakk0M')
+#print(E77.__vfi.p)
+#
+################################################################################
+#
+#model = '1'
+#cf_key = 'diffsq'
+#params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
+#fixed = [3e-2, None, 3.76e-6, None, 1.156e-6, 16., 27.179]
+#vvm.fitter(E76, params0, fixed, model=model, profiles='all', cf_key=cf_key)
+#assess_vvm_fit(E76, str(E76.floatID)+'_fix_alphakk0V0alphapM')
+#print(E76.__vfi.p)
+#
+#model = '1'
+#cf_key = 'diffsq'
+#params0 = np.array([3e-2, 5e-2, 3e-6, 4e+2, 1e-6, 16., 27.179])
+#fixed = [3e-2, None, 3.76e-6, None, 1.156e-6, 16., 27.179]
+#vvm.fitter(E77, params0, fixed, model=model, profiles='all', cf_key=cf_key)
+#assess_vvm_fit(E77, str(E77.floatID)+'_fix_alphakk0V0alphapM')
+#print(E77.__vfi.p)
