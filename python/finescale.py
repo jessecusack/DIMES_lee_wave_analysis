82ff9adcab069c8796c7fa2faf544e20b42a967e
jessecusack <jesse.cusack@noc.soton.ac.uk>
2014-05-30 15:35:26 +0100

    added apply strain function

--- python/finescale.py
+++ python/finescale.py
@@ -34,7 +34,7 @@ def adiabatic_level(P, SA, T, lat, P_bin_width=200., deg=1):
 
       Returns
       -------
-      N2_ref : 1-D ndarray of floats
+      N2_ref : 1-D ndarray
           Reference buoyancy frequency [s-2]
 
       Raises
@@ -42,66 +42,8 @@ def adiabatic_level(P, SA, T, lat, P_bin_width=200., deg=1):
 
       Notes
       -----
-
-      Examples
-      --------
-
-    """
-
-    N2_ref = np.NaN*P.copy()
-    P_min, P_max = np.nanmin(P), np.nanmax(P)
-    pot_rho = gsw.pot_rho_t_exact
-
-    for i in xrange(len(P)):
-        P_bin_min = np.maximum(P[i] - P_bin_width/2., P_min)
-        P_bin_max = np.minimum(P[i] + P_bin_width/2., P_max)
-        in_bin = np.where((P >= P_bin_min) & (P <= P_bin_max))[0]
-        if in_bin.size == 0:
-            continue
-        P_bar = np.nanmean(P[in_bin])
-        T_bar = np.nanmean(T[in_bin])
-        SA_bar = np.nanmean(SA[in_bin])
-        rho_bar = pot_rho(SA_bar, T_bar, P_bar, P_bar)
-        sv = 1./pot_rho(SA[in_bin], T[in_bin], P[in_bin], P_bar)
-
-        p = np.polyfit(P[in_bin], sv - np.nanmean(sv), deg)
-        g = gsw.grav(lat, P_bar)
-        N2_ref[i] = -1e-4 * rho_bar**2 * g**2 * p[0]
-
-    return N2_ref
-
-
-def adiabatic_level2(P, SA, T, lat, P_bin_width=200., deg=1):
-    """Generate smooth buoyancy frequency profile by applying the adiabatic
-    levelling method of Bray and Fofonoff (1981).
-
-    TODO: Add optional parameter ignore_above.
-
-      Parameters
-      ----------
-      P : 1-D ndarray
-          Pressure [dbar]
-      SA : 1-D ndarray
-          Absolute salinity [g/kg]
-      T : 1-D ndarray
-          Temperature [degrees C]
-      lat : float
-          Latitude [-90...+90]
-      p_bin_width : float, optional
-          Pressure bin width [dbar]
-      deg : int, option
-          Degree of polynomial fit.
-
-      Returns
-      -------
-      N2_ref : 1-D ndarray of floats
-          Reference buoyancy frequency [s-2]
-
-      Raises
-      ------
-
-      Notes
-      -----
+      Calls to the gibbs seawater toolbox are slow and therefore this function
+      is quite slow.
 
       Examples
       --------
@@ -110,6 +52,10 @@ def adiabatic_level2(P, SA, T, lat, P_bin_width=200., deg=1):
 
     N2_ref = np.NaN*P.copy()
     nans = np.isnan(P) | np.isnan(SA) | np.isnan(T)
+    
+    # If there are nothing but NaN values don't waste time.
+    if np.sum(nans) == nans.size:
+        return N2_ref
     
     P = P[~nans]
     SA = SA[~nans]
@@ -132,14 +78,15 @@ def adiabatic_level2(P, SA, T, lat, P_bin_width=200., deg=1):
         SAm[in_bin, i] = SA[in_bin]
         Tm[in_bin, i] = T[in_bin]
         
-        
     P_bar = np.nanmean(Pm, axis=0)
     T_bar = np.nanmean(Tm, axis=0)
     SA_bar = np.nanmean(SAm, axis=0)
+    
+    # Perform thermodynamics once only... 
     rho_bar = gsw.pot_rho_t_exact(SA_bar, T_bar, P_bar, P_bar)
     sv = 1./gsw.pot_rho_t_exact(SAm, Tm, Pm, P_bar)
     
-    p = []    
+    p = []
     for P_bin, sv_bin in zip(Pm.T, sv.T):
         bnans = np.isnan(P_bin)
         p.append(np.polyfit(P_bin[~bnans], 
@@ -152,3 +99,73 @@ def adiabatic_level2(P, SA, T, lat, P_bin_width=200., deg=1):
     N2_ref[~nans] = -1e-4*rho_bar**2*g**2*p[:,0]
 
     return N2_ref
+
+
+def adiabatic_level2(P, SA, T, lat, P_bin_width=100.):
+    """Generate smooth buoyancy frequency profile by applying the adiabatic
+    levelling method of Bray and Fofonoff (1981) to gridded data where each 
+    column is a profile.
+
+    TODO: Add optional parameter ignore_above.
+
+      Parameters
+      ----------
+      P : 2-D ndarray
+          Pressure [dbar]
+      SA : 2-D ndarray
+          Absolute salinity [g/kg]
+      T : 2-D ndarray
+          Temperature [degrees C]
+      lat : float
+          Latitude [-90...+90]
+      p_bin_width : float, optional
+          Pressure bin width [dbar]
+      deg : int, option
+          Degree of polynomial fit.
+
+      Returns
+      -------
+      N2_ref : 1-D ndarray
+          Reference buoyancy frequency [s-2]
+
+      Raises
+      ------
+
+      Notes
+      -----
+      Calls to the gibbs seawater toolbox are slow and therefore this function
+      is quite slow.
+
+      Examples
+      --------
+
+    """
+    
+    pass
+
+    
+def apply_strain(Float, P_bin_width=100.):
+    """
+    Takes a float and a pressure bin width then calculates N2_ref for all 
+    available profiles and sets N2_ref and strain_z as attributes of the 
+    float class.
+    
+    """
+
+    Pg = getattr(Float, 'P')
+    SAg = getattr(Float, 'SA')
+    Tg = getattr(Float, 'T')
+    lats = getattr(Float, 'lat_start')
+    
+    N2_ref = np.NaN*Pg.copy()
+    
+    for i, (P, SA, T, lat) in enumerate(zip(Pg.T, SAg.T, Tg.T, lats)):
+        print("hpid: {}".format(Float.hpid[i]))
+        N2_ref[:, i] = adiabatic_level(P, SA, T, lat, P_bin_width)
+        
+    N2 = getattr(Float, 'N2')
+    strain_z = (N2 - N2_ref)/N2_ref
+        
+    setattr(Float, 'N2_ref', N2_ref)
+    setattr(Float, 'strain_z', strain_z)
+        
