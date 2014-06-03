8640c2d0d32499f2d6af5ad725b8567a176aab1b
jessecusack <jesse.cusack@noc.soton.ac.uk>
2014-06-03 14:46:31 +0100

    noted down intentions

--- python/large_wave_analysis.py
+++ python/large_wave_analysis.py
@@ -6,3 +6,8 @@ Created on Wed Apr 16 16:48:46 2014
 """
 
 import scipy.signal as sig
+
+# Lets start by diagnosing wave properties such as when, where, bathymetry, 
+# timing differences between two floats, float track.
+
+# Lets attempt this in an iPython notebook...
