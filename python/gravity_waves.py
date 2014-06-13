4c5373f20bfbf624e048adfea6f29d6324d0f5e4
jessecusack <jesse.cusack@noc.soton.ac.uk>
2014-06-13 17:02:04 +0100

    new module for gravity wave related functions

---
+++ python/gravity_waves.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 13 16:31:11 2014
+
+@author: jc3e13
+
+This module contains functions for investigating internal gravity waves.
+
+"""
+
+import numpy as np
+
+
+def omega(N, k, m, l=None, f=None):
+    """Dispersion relation for an internal gravity wave in a continuously 
+    stratified fluid.
+
+      Parameters
+      ----------
+      N : ndarray
+          Buoyancy frequency [s-1]
+      k : ndarray
+          Horizontal wavenumber (x) [m-1]
+      m : ndarray
+          Vertical wavenumber (z) [m-1]
+      l : ndarray, optional
+          Horizontal wavenumber (y) [m-1]
+      f : ndarray, optional
+          Coriolis parameter [s-1]
+
+      Returns
+      -------
+      omega : ndarray
+          Frequency [s-1]
+
+      Raises
+      ------
+
+      Notes
+      -----
+
+      Examples
+      --------
+    
+    
+    """
+    
+    N2 = N**2
+    k2 = k**2
+    m2 = m**2
+    
+    if l is None and f is None:
+        # 2D case without rotation.
+        return np.sqrt(N2*k2/(k2 + m2))
+    elif l is not None and f is None:
+        # 3D case without rotation.
+        l2 = l**2
+        return np.sqrt(N2*(k2 + l2)/(k2 + l2 + m2))
+    elif l is None and f is not None:
+        # 2D case with rotation.
+        f2 = f**2
+        return np.sqrt((f2*m2 + N2*k2)/(k2 + m2))
+    elif l is not None and f is not None:
+        # 3D case with rotation.
+        f2 = f**2
+        l2 = l**2
+        return np.sqrt((f2*m2 + N2*(k2 + l2))/(k2 + l2 + m2))
