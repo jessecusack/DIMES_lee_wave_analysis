6141b8a108626f9b311dae90809a283a7ca79eb7
jessecusack <jesse.cusack@noc.soton.ac.uk>
2014-07-14 14:41:57 +0100

    will move garrett-munk stuff into another module

--- python/finescale.py
+++ python/finescale.py
@@ -143,27 +143,27 @@ def L(f, N):
     return f*np.arccosh(N/f)/(f30*np.arccosh(N0/f30))
 
 
-def GM(omega, f, N):
-    """Garrett-Munk (power?) spectrum.
-
-    Model variant for the frequency spectrum of horizontal kinetic energy.
-    """
-
-    om2 = omega**2.
-    f2 = f**2.
-    N2 = N**2.
-
-    a = 0.5
-    b = 0.
-
-    continuum = f*(om2 + f2)*(N2 - om2)/(N2*om2**2.)
-    inertial = om2/(om2 - f2)**a
-    buoyancy = N2/(N2 - om2)**b
-
-    return continuum*inertial*buoyancy
-
-def GM2():
-    pass
+#def GM(omega, f, N):
+#    """Garrett-Munk (power?) spectrum.
+#
+#    Model variant for the frequency spectrum of horizontal kinetic energy.
+#    """
+#
+#    om2 = omega**2.
+#    f2 = f**2.
+#    N2 = N**2.
+#
+#    a = 0.5
+#    b = 0.
+#
+#    continuum = f*(om2 + f2)*(N2 - om2)/(N2*om2**2.)
+#    inertial = om2/(om2 - f2)**a
+#    buoyancy = N2/(N2 - om2)**b
+#
+#    return continuum*inertial*buoyancy
+#
+#def GM2():
+#    pass
 # betastar=pi*jstar/b*sqrt(N2mean)/N0;
 # phi_zeta = E*b^3*N0^2/(2*jstar*pi*N2mean) ./(1+kzax/betastar).^2; % power spectral density of vertical displacement
 # phi_eta = kzax.^2.*phi_zeta; % power spectral density of vertical strain
