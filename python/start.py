85ea88f9c0ca69a3992ff4d4bdc00690a5e9918d
jessecusack <jesse.cusack@noc.soton.ac.uk>
2014-07-25 22:23:12 +0100

    updated startup file

--- python/start.py
+++ python/start.py
@@ -9,14 +9,21 @@ Created on Thu May 29 14:52:32 2014
 
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy as sp
 import plotting_functions as pf
-import gsw
 import emapex
+import gsw
+import os
+import sandwell
+from scipy.interpolate import griddata
+from scipy.integrate import cumtrapz
+import scipy.signal as sig
 
+reload(emapex)
 
 E76 = emapex.EMApexFloat('../../data/EM-APEX/allprofs11.mat', 4976)
 E76.apply_w_model('../../data/EM-APEX/4976_fix_p0k0M_fit_info.p')
+E76.apply_strain('../../data/EM-APEX/4976_N2_ref_300dbar.p')
 
 E77 = emapex.EMApexFloat('../../data/EM-APEX/allprofs11.mat', 4977)
 E77.apply_w_model('../../data/EM-APEX/4977_fix_p0k0M_fit_info.p')
+E77.apply_strain('../../data/EM-APEX/4977_N2_ref_300dbar.p')
