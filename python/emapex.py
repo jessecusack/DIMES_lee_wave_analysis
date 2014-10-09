7501bf8e3c8349a6915431929584de5d0f0b8f19
jessecusack <jesse.cusack@noc.soton.ac.uk>
2014-10-09 16:04:02 +0100

    flip function moved from emapex to utils

--- python/emapex.py
+++ python/emapex.py
@@ -718,59 +718,6 @@ def up_down_indices(hpid_array, up_or_down='up'):
         raise RuntimeError('Inputs are probably wrong.')
 
 
-def flip_cols(data, cols=None):
-    """Input an array of data. Receive flipped array. If array is two
-    dimensional then a list of columns should be provided else the whole matrix
-    will be flipped. This is different from the numpy.flipud function because
-    any end chunk of data won't be flipped, e.g.:
-
-    [1, 2, 3, nan, nan] -> [3, 2, 1, nan, nan]
-
-    also
-
-    [1, 2, nan, 4, nan, nan] -> [4, nan, 2, 1, nan, nan]
-
-    If data is 2D, assumes that each column contains a list of data otherwise
-    please transpose the input.
-
-    Sometimes data sets combine combine columns of different lengths and pad
-    out the empty space with nans. This is useful for flipping in that
-    situation.
-
-    """
-    out_data = data.copy()
-    d = np.ndim(data)
-
-    def flip(arr):
-        flip_arr = np.flipud(arr)
-        nnans = ~np.isnan(flip_arr)
-        idx = nnans.searchsorted(True)
-#        try:
-#            indx = next(i for i, el, in enumerate(flip_arr) if ~np.isnan(el))
-#        except StopIteration:
-#            return flip_arr
-        return np.concatenate((flip_arr[idx:], flip_arr[:idx]))
-
-    if d == 1 and cols is None:
-
-        out_data = flip(data)
-
-    elif d == 2 and cols is not None:
-
-        for col_indx, col in zip(cols, data[:, cols].T):
-            out_data[:, col_indx] = flip(col)
-
-    elif d == 2 and cols is None:
-
-        for col_indx, col in enumerate(data.T):
-            out_data[:, col_indx] = flip(col)
-
-    else:
-        raise RuntimeError('Inputs are probably wrong.')
-
-    return out_data
-
-
 def what_floats_are_in_here(fname):
     """Finds all unique float ID numbers from a given allprofs##.mat file."""
     fs = io.loadmat(fname, squeeze_me=True, variable_names='flid')['flid']
--- python/utils.py
+++ python/utils.py
@@ -88,3 +88,56 @@ def lldist(lon, lat):
     angles = 2.*np.arctan2(np.sqrt(a), np.sqrt(1-a))
     dist = earth_radius*angles
     return dist
+
+
+def flip_cols(data, cols=None):
+    """Input an array of data. Receive flipped array. If array is two
+    dimensional then a list of columns should be provided else the whole matrix
+    will be flipped. This is different from the numpy.flipud function because
+    any end chunk of data won't be flipped, e.g.:
+
+    [1, 2, 3, nan, nan] -> [3, 2, 1, nan, nan]
+
+    also
+
+    [1, 2, nan, 4, nan, nan] -> [4, nan, 2, 1, nan, nan]
+
+    If data is 2D, assumes that each column contains a list of data otherwise
+    please transpose the input.
+
+    Sometimes data sets combine combine columns of different lengths and pad
+    out the empty space with nans. This is useful for flipping in that
+    situation.
+
+    """
+    out_data = data.copy()
+    d = np.ndim(data)
+
+    def flip(arr):
+        flip_arr = np.flipud(arr)
+        nnans = ~np.isnan(flip_arr)
+        idx = nnans.searchsorted(True)
+#        try:
+#            indx = next(i for i, el, in enumerate(flip_arr) if ~np.isnan(el))
+#        except StopIteration:
+#            return flip_arr
+        return np.concatenate((flip_arr[idx:], flip_arr[:idx]))
+
+    if d == 1 and cols is None:
+
+        out_data = flip(data)
+
+    elif d == 2 and cols is not None:
+
+        for col_indx, col in zip(cols, data[:, cols].T):
+            out_data[:, col_indx] = flip(col)
+
+    elif d == 2 and cols is None:
+
+        for col_indx, col in enumerate(data.T):
+            out_data[:, col_indx] = flip(col)
+
+    else:
+        raise RuntimeError('Inputs are probably wrong.')
+
+    return out_data
