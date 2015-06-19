aa091c7cb708abc430f983ea18544d8a8ce8d64f
jessecusack <jesse.cusack@noc.soton.ac.uk>
2015-06-19 17:01:58 +0100

    plots added to flux analysis

--- scripts/flux_analysis.py
+++ scripts/flux_analysis.py
@@ -9,6 +9,8 @@ import numpy as np
 import scipy as sp
 import sys
 import os
+import glob
+import matplotlib
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
@@ -16,8 +18,10 @@ lib_path = os.path.abspath('../modules')
 if lib_path not in sys.path:
     sys.path.append(lib_path)
 
+import sandwell
 import emapex
 import utils
+import plotting_functions as pf
 
 
 try:
@@ -31,21 +35,47 @@ E76.update_profiles()
 E77.calculate_pressure_perturbation()
 E77.update_profiles()
 
+# %% Script params.
+
+# Bathymetry file path.
+bf = os.path.abspath(glob.glob('/noc/users/jc3e13/storage/smith_sandwell/topo_*.img')[0])
+# Figure save path.
+sdir = '../figures/flux analysis'
+if not os.path.exists(sdir):
+    os.makedirs(sdir)
+# Universal figure font size.
+matplotlib.rc('font', **{'size': 9})
 
 # %%
 
 # The portions of the profiles that contain the wave. Found by eye.
-zlims = {4976: {29: (-1600, -200),
-                30: (-1000, -200),
-                31: (-1600, -600),
-                32: (-1600, -400)},
-         4977: {24: (-1600, -200),
-                25: (-1400, -600),
-                26: (-1600, -600),
-                27: (-1200, -200)}} # was -1200 m min
+zlims = {4976: {25: (-1600, -100),
+                26: (-1600, -100),
+                27: (-1600, -100),
+                28: (-1600, -100),
+                29: (-1600, -200),  # 1
+                30: (-1000, -200),  # 2
+                31: (-1600, -600),  # 3
+                32: (-1600, -400),  # 4
+                33: (-1600, -100),
+                34: (-1600, -100),
+                35: (-1600, -100),
+                36: (-1600, -100)},
+         4977: {20: (-1600, -100),
+                21: (-1600, -100),
+                22: (-1600, -100),
+                23: (-1600, -100),
+                24: (-1600, -200),  # 1
+                25: (-1400, -600),  # 2
+                26: (-1600, -600),  # 3
+                27: (-1200, -200),  # 4
+                28: (-1600, -100),
+                29: (-1600, -100),
+                30: (-1600, -100),
+                31: (-1600, -100)}}
 
-hpids_76 = np.array([29, 30, 31, 32])
-hpids_77 = np.array([24, 25, 26, 27])
+hpids_76 = np.array([25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36])
+hpids_77 = np.array([20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31])
 
 #hpids_76 = np.array([31])
 #hpids_77 = np.array([26])
@@ -66,8 +96,18 @@ deg = 1
 #ax2.set_xlim(-0.3, 0.3)
 #ax2.set_ylim(-0.3, 0.3)
 
-i = 0.
+
+uwbar = np.zeros((2, N/2))
+vwbar = np.zeros((2, N/2))
+pwbar = np.zeros((2, N/2))
+Uuwbar = np.zeros((2, N/2))
+Vvwbar = np.zeros((2, N/2))
+tau = np.zeros((2, N/2))
+E = np.zeros((2, N/2))
+
+i = 0
 for Float, hpids in zip([E76, E77], [hpids_76, hpids_77]):
+    j = 0
     for hpid in hpids:
 
         zmin, zmax = zlims[Float.floatID][hpid]
@@ -111,37 +151,37 @@ for Float, hpids in zip([E76, E77], [hpids_76, hpids_77]):
         v = utils.nan_detrend(z, v, deg)
         pp = utils.nan_detrend(z, pp, deg)
 
-        plt.figure()
-        plt.plot(u, z, v, z, w, z)
-        plt.xlim(-0.4, 0.4)
-        plt.ylim(-1500., 0.)
-        plt.title("Float {}. hpid {}.".format(pfl.floatID, pfl.hpid[0]))
+#        plt.figure()
+#        plt.plot(u, z, v, z, w, z)
+#        plt.xlim(-0.4, 0.4)
+#        plt.ylim(-1500., 0.)
+#        plt.title("Float {}. hpid {}.".format(pfl.floatID, pfl.hpid[0]))
 
         DT = np.max(tef) - np.min(tef)
 
         print("\nFloat {}. hpid {}.".format(pfl.floatID, pfl.hpid[0]))
 
-        uwbar = rho0*sp.integrate.trapz(w*u, tef)/DT
-        vwbar = rho0*sp.integrate.trapz(w*v, tef)/DT
-        pwbar = rho0*sp.integrate.trapz(w*pp, tef)/DT
+        uwbar[i, j] = rho0*sp.integrate.trapz(w*u, tef)/DT
+        vwbar[i, j] = rho0*sp.integrate.trapz(w*v, tef)/DT
+        pwbar[i, j] = rho0*sp.integrate.trapz(w*pp, tef)/DT
 
-        Uuwbar = -U*uwbar
-        Vvwbar = -V*vwbar
+        Uuwbar[i, j] = -U*uwbar[i, j]
+        Vvwbar[i, j] = -V*vwbar[i, j]
 
-        tau = np.sqrt(uwbar**2 + vwbar**2)
+        tau[i, j] = np.sqrt(uwbar[i, j]**2 + vwbar[i, j]**2)
 
         kinetic = 0.5*rho0*sp.integrate.trapz(u**2 + v**2 + w**2, tef)/DT
         potential = 0.5*rho0*sp.integrate.trapz(b**2/N2mean, tef)/DT
-        E = kinetic + potential
+        E[i, j] = kinetic + potential
 
-        print("Reynolds stress: {:1.2f} N m-2".format(tau))
-        print("Components: ({:1.2f}, {:1.2f}) N m-2".format(uwbar, vwbar))
-        print("Energy density: {:1.2f} J m-3".format(E))
+        print("Reynolds stress: {:1.2f} N m-2".format(tau[i, j]))
+        print("Components: ({:1.2f}, {:1.2f}) N m-2".format(uwbar[i, j], vwbar[i, j]))
+        print("Energy density: {:1.2f} J m-3".format(E[i, j]))
         print("Standard deviation of pressure perturbation: "
               "{:1.2f} m2 s-2".format(np.std(pp)))
-        print("Vertical energy flux: {:1.2f} W m-2".format(pwbar))
+        print("Vertical energy flux: {:1.2f} W m-2".format(pwbar[i, j]))
         print("Vertical energy from from (Uuw, Vvw): ({:1.2f}, {:1.2f})"
-              "W m-2 ".format(Uuwbar, Vvwbar))
+              "W m-2 ".format(Uuwbar[i, j], Vvwbar[i, j]))
 
 #        print(1025.*sp.integrate.trapz(v*u, z)/(z[0]-z[-1]))
 
@@ -151,6 +191,46 @@ for Float, hpids in zip([E76, E77], [hpids_76, hpids_77]):
 #                   scale_units='xy', angles='xy', scale=1,
 #                   color=mpl.cm.Set1(i/N))
 
-        i += 1
+        j += 1
+    i += 1
 
-#ax2.grid()
+fig, axs = plt.subplots(3, 1, sharex=True, figsize=(3.125, 5))
+
+ds = []
+
+for Float, hpids in zip([E76, E77], [hpids_76, hpids_77]):
+
+    __, idxs = Float.get_profiles(hpids, ret_idxs=True)
+
+    lon = getattr(Float, 'lon_start')[idxs]
+    lat = getattr(Float, 'lat_start')[idxs]
+    d = getattr(Float, 'dist')[idxs]
+
+    bathy = sandwell.interp_track(lon, lat, bf)
+
+    d -= d[bathy.argmax()]
+    ds.append(d.copy())
+
+axs[2].set_xlabel('Distance from ridge top (km)', labelpad=0.06)
+axs[2].set_ylabel('$z$ (m)')
+
+axs[2].fill_between(d, bathy, np.nanmin(bathy), color='black',
+                    linewidth=2)
+axs[2].set_ylim(np.nanmin(bathy), np.nanmax(bathy))
+axs[2].set_xlim(np.nanmin(d), np.nanmax(d))
+
+axs[1].plot(ds[0], uwbar[0, :], 'b:')
+axs[1].plot(ds[0], vwbar[0, :], 'b--')
+axs[1].plot(ds[0], tau[0, :], 'b-')
+axs[1].plot(ds[1], uwbar[1, :], 'g:')
+axs[1].plot(ds[1], vwbar[1, :], 'g--')
+axs[1].plot(ds[1], tau[1, :], 'g-')
+
+axs[1].set_ylabel("$<u'w'>$ (N m$^{-2}$)")
+
+axs[0].plot(ds[0], Uuwbar[0, :] + Vvwbar[0, :], label='4976')
+axs[0].plot(ds[1], Uuwbar[1, :] + Vvwbar[1, :], label='4977')
+axs[0].set_ylabel("$U<u'w'>$ (W m$^{-2}$)")
+axs[0].legend(loc=0)
+
+pf.my_savefig(fig, 'both', 'fluxes', sdir, ftype='png', fsize='single_col')
