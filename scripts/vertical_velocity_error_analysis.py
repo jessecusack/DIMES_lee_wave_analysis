d4e9746fc8ae05bc693b26915cf030b7514d5a55
jessecusack <jesse.cusack@noc.soton.ac.uk>
2015-08-06 16:34:14 +0100

    new spectra comparison added

--- scripts/vertical_velocity_error_analysis.py
+++ scripts/vertical_velocity_error_analysis.py
@@ -113,6 +113,142 @@ print('Error estimate: {} m s-1'.format(np.sqrt(pmin*m.max())))
 #pfit = np.polyfit(np.log(m[idxs]), np.log(Pw[idxs]), 1)
 #plt.loglog(m, m**pfit[0]/np.mean(m[idxs]**pfit[0]/Pw[idxs]))
 
+# %% Investigation using the lomb-scargle method without interpolation and
+# then with interpolation.
+
+# Choose float and range of hpids.
+hpids = np.arange(10, 70)
+Float = E76
+pfls = Float.get_profiles(hpids)
+
+# Chop ends where model is invalid.
+zmax = -30.
+zmin = -1400.
+
+# Choose frequencies to estimate power.
+tdmax = 9000.
+tdmin = 20.
+dzmax = 1200.
+dzmin = 4.
+N = 100
+om = np.logspace(np.log10(np.pi*2/tdmax), np.log10(np.pi*2/tdmin), N)
+m = np.logspace(np.log10(np.pi*2/dzmax), np.log10(np.pi*2/dzmin), N)
+
+om_pgrams = np.empty((om.size, pfls.size))
+m_pgrams = np.empty((m.size, pfls.size))
+
+for i, pfl in enumerate(pfls):
+    nans = np.isnan(pfl.Ww)
+    w = pfl.Ww[~nans]
+    t = pfl.UTC[~nans]*24*60*60
+    z = pfl.z[~nans]
+
+    invalid = (z > zmax) & (z < zmin)
+    w = w[~invalid]
+    t = t[~invalid]
+    z = z[~invalid]
+
+    om_pgrams[:, i] = sp.signal.lombscargle(t, w, om)/om
+    m_pgrams[:, i] = sp.signal.lombscargle(t, w, m)/m
+
+f = om/(np.pi*2)
+m = m/(np.pi*2)
+
+fig1, ax1 = plt.subplots(1, 1)
+ax1.loglog(1./f, om_pgrams, color='blue', alpha=0.1)
+ax1.loglog(1./f, np.mean(om_pgrams, axis=-1), color='black',
+         linewidth=3.)
+ax1.set_xlabel('$\omega$ (rad s$^{-1})$')
+
+fig2, ax2 = plt.subplots(1, 1)
+plt.loglog(1./m, m_pgrams, color='blue', alpha=0.1)
+plt.loglog(1./m, np.mean(m_pgrams, axis=-1), color='black',
+         linewidth=3.)
+plt.xlabel('$m$ (rad m$^{-1}$)')
+
+
+######## Using Interpolation #########
+
+# Choose float and range of hpids.
+#hpids = np.arange(50, 150)
+#Float = E76
+#pfls = Float.get_profiles(hpids)
+scaling = 'density'
+
+# Chop ends where model is invalid.
+zmax = -30.
+zmin = -1400.
+tmax = 9000.
+tmin = 200.
+tdmin = 20.
+dzmin = 4.
+
+# Interpolation points.
+N = 2**11
+ti, dt = np.linspace(tmin, tmax, N, retstep=True)
+zi, dz = np.linspace(zmin, zmax, N, retstep=True)
+
+om_pgrams = np.empty((ti.size/2 + 1, pfls.size))
+m_pgrams = np.empty((zi.size/2 + 1, pfls.size))
+
+for i, pfl in enumerate(pfls):
+    nans = np.isnan(pfl.Ww)
+    w = pfl.Ww[~nans]
+    t = pfl.UTC[~nans]*24*60*60
+    z = pfl.z[~nans]
+
+    invalid = (z > zmax) & (z < zmin)
+    w = w[~invalid]
+    t = t[~invalid]
+    z = z[~invalid]
+
+    wit = np.interp(ti, t - t[0], w)
+    wiz = np.interp(zi, z, w)
+
+    f, om_pgrams[:, i] = sp.signal.periodogram(wit, fs=1./dt, scaling=scaling)
+    m, m_pgrams[:, i] = sp.signal.periodogram(wiz, fs=1./dz, scaling=scaling)
+
+# Chop interpolation noise.
+fuse = f < 1./tdmin
+muse = m < 1./dzmin
+f, om_pgrams = f[fuse], om_pgrams[fuse, :]
+m, m_pgrams = m[muse], m_pgrams[muse, :]
+om_pgrams[0, :] = 0.
+m_pgrams[0, :] = 0.
+
+# Convert to radian units.
+ax1.loglog(1./f, om_pgrams, color='red', alpha=0.1)
+ax1.loglog(1./f, np.mean(om_pgrams, axis=-1), color='black',
+         linewidth=3.)
+ax1.set_xlabel('$T$ (s)')
+ax1.grid()
+
+ax2.loglog(1./m, m_pgrams, color='red', alpha=0.1)
+ax2.loglog(1./m, np.mean(m_pgrams, axis=-1), color='black',
+           linewidth=3.)
+ax2.set_xlabel('$\lambda$ (m)')
+ax2.grid()
+
+
+def tick_func(ticks):
+    return ["{:.0f}".format(tick) for tick in ticks]
+
+ax1.set_xticklabels(tick_func(ax1.get_xticks()))
+ax2.set_xticklabels(tick_func(ax2.get_xticks()))
+
+#ax1t = ax1.twiny()
+#ax1t.set_xticks(np.log10(ax1.get_xticks()))
+#ax1t.set_xticklabels(tick_func(1./ax1.get_xticks()))
+##ax1t.set_xbound(ax1.get_xbound())
+#ax1t.set_xlabel("T (s)")
+#
+#ax2t = ax2.twiny()
+#L_ticks = np.logspace(4, 0, 5)
+#ax2t.set_xticks(np.log10(1./L_ticks))
+#ax2t.set_xticklabels(tick_func(L_ticks))
+##ax2t.set_xbound(ax2.get_xbound())
+#ax2t.set_xlabel("$\lambda$ (m)")
+
 # %% Error due to parameter uncertainty
 
 Float = E76
